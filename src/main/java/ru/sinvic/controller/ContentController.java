e53df7775b3ff6a603742bd1c6df7fe56cbfffe4
Victoria <vicktoria.sin@yandex.ru>
2026-02-13 22:27:38 +0300

    add content controller

---
+++ src/main/java/ru/sinvic/controller/ContentController.java
@@ -0,0 +1,34 @@
+package ru.sinvic.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.sinvic.domain.Content;
+import ru.sinvic.repository.ContentRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/content")
+public class ContentController {
+
+    private final ContentRepository contentRepository;
+
+    public ContentController(ContentRepository contentRepository) {
+        this.contentRepository = contentRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Content>> getAllContent() {
+        return ResponseEntity.ok(contentRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Content> getContent(@PathVariable Long id) {
+        return contentRepository.findByIdWithTimeline(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
+    }
+}
