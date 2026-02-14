89b8cc740c59ac43b1d6c85b87993ac7b67fe583
Victoria <vicktoria.sin@yandex.ru>
2026-02-15 00:15:47 +0300

    add video

---
+++ src/main/java/ru/sinvic/MediaPipelineApplication.java
@@ -0,0 +1,12 @@
+package ru.sinvic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaPipelineApplication.class, args);
+    }
+}
