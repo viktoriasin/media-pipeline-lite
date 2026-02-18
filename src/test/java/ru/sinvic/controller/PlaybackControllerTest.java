623730bf5d766a03a1a85c601721dea6d7924e2a
Victoria <vicktoria.sin@yandex.ru>
2026-02-18 13:53:35 +0300

    delete unused imports

--- src/test/java/ru/sinvic/controller/PlaybackControllerTest.java
+++ src/test/java/ru/sinvic/controller/PlaybackControllerTest.java
@@ -1,7 +1,6 @@
 package ru.sinvic.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
